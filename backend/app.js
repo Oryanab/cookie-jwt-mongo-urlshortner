f8a79607075c56227cba174eb0366cbd8f00d7ba
Oryanab <86202122+Oryanab@users.noreply.github.com>
2021-11-04 10:40:54 +0200

    clean deployment

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
+//process.env.PORT ||
 const shorturlRouter = require("./routers/shorturl");
 const redirectRouter = require("./routers/cybr");
 const userRouter = require("./routers/signUsers");
