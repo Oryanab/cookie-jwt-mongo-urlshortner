39a8ce37830475a5940b061bbc8fb70b8fa686fe
Oryanab <86202122+Oryanab@users.noreply.github.com>
2021-11-16 09:54:05 +0200

    update 6.0

--- backend/routers/signUsers.js
+++ backend/routers/signUsers.js
@@ -131,16 +131,22 @@ userRouter.get("/check", authenticateToken, (req, res) => {
   res.json({ username: req.user.name });
 });
 // Auth jwt middleware
+
 function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token == null) return res.sendStatus(401);
-  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-    //console.log(err);
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
+  if (token === null) {
+    return res.sendStatus(401);
+  } else {
+    jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      } else {
+        req.user = user;
+        next();
+      }
+    });
+  }
 }
 
 module.exports = userRouter;
